bf5c32c1fd1a80ca12830cd1e63b4c67d279bd62
tutorialcreation <tutorialcreation81@gmail.com>
2024-01-23 13:23:24 +0300

    testing

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -147,10 +147,10 @@ PASSWORD_HASHERS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Dhaka'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
