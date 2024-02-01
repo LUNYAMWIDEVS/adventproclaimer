02bc9588a813d4abfd97627cf6095638657218e3
lunyamwis <lutherlunyamwi@gmail.com>
2024-02-01 16:39:44 +0300

    returned it back to original

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DBNAME").strip(),
-        "USER": os.getenv("POSTGRES_USERNAME","postgres").strip(),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD","adventproclaimer").strip(),
+        "USER": os.getenv("POSTGRES_USERNAME").strip(),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD").strip(),
         "HOST": os.getenv("POSTGRES_HOST").strip(),
         "PORT": 5432,
     }
