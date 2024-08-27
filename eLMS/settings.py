0f98677fed411c8c2a15d34dffe7311ab37e41ac
lunyamwis <lutherlunyamwi@gmail.com>
2024-08-27 14:09:26 +0300

    removing sha1

--- eLMS/settings.py
+++ eLMS/settings.py
@@ -186,7 +186,7 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    # 'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedSHA1PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
